5a21942c9758f0f8524183050cb0487fbcf8947f
Alexander Smart <aasmart@Alexanders-MacBook-Pro.local>
2025-12-02 12:49:38 -0500

    part 2

--- day2/day.go
+++ day2/day.go
@@ -24,7 +24,10 @@ func numDigitsInNum(num int) int {
 	return digits
 }
 
-func sumInvalidIdsInRange(lower int, upper int) int {
+func sumInvalidIdsInRange(lowerStr string, upperStr string) int {
+	lower, _ := strconv.Atoi(lowerStr)
+	upper, _ := strconv.Atoi(upperStr)
+
 	totalInvalidNums := 0
 
 	numDigitsInLower := numDigitsInNum(lower)
@@ -52,7 +55,37 @@ func sumInvalidIdsInRange(lower int, upper int) int {
 	return totalInvalidNums
 }
 
-func part1(filename string) {
+func sumInvalidIsInRangePart2(lower string, upper string) int {
+	totalInvalidNums := 0
+
+	lowerNum, _ := strconv.Atoi(lower)
+	upperNum, _ := strconv.Atoi(upper)
+	currNum := lowerNum
+	for currNum <= upperNum {
+		currNumStr := strconv.Itoa(currNum)
+		currNumLength := len(currNumStr)
+
+		for i := range currNumLength / 2 {
+			repeatStr := currNumStr[:i+1]
+			if currNumLength%(i+1) != 0 {
+				continue
+			}
+
+			num, _ := strconv.Atoi(strings.Repeat(repeatStr, currNumLength/(i+1)))
+
+			if num == currNum {
+				totalInvalidNums += num
+				break
+			}
+		}
+
+		currNum += 1
+	}
+
+	return totalInvalidNums
+}
+
+func processInput(filename string, id_check func(string, string) int) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -69,15 +102,13 @@ func part1(filename string) {
 	ranges := strings.SplitSeq(line, ",")
 	for r := range ranges {
 		splitRange := strings.Split(r, "-")
-		lower, _ := strconv.Atoi(splitRange[0])
-		upper, _ := strconv.Atoi(splitRange[1])
-
-		totalInvalidIds += sumInvalidIdsInRange(lower, upper)
+		totalInvalidIds += id_check(splitRange[0], splitRange[1])
 	}
 
 	fmt.Println(totalInvalidIds)
 }
 
 func main() {
-	part1(os.Args[1])
+	processInput(os.Args[1], sumInvalidIdsInRange)
+	processInput(os.Args[1], sumInvalidIsInRangePart2)
 }
