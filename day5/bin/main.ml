b2051f82cb120aba00b592a35aec05be93e461d2
Alexander Smart <aasmart@umich.edu>
2025-12-05 18:29:05 -0500

    day 5 pt 2
    im less bad now

--- day5/bin/main.ml
+++ day5/bin/main.ml
@@ -55,12 +55,58 @@ let count_valid_ids ((lines, interval_map) : String.t list * interval_list)
   res
 ;;
 
+let rec merge_intervals
+          ((interval_start, interval_end) : Interval.t)
+          (intervals : interval_list)
+  : interval_list
+  =
+  match intervals with
+  | [] -> (interval_start, interval_end) :: []
+  | (hd_start, hd_end) :: tl ->
+    if hd_start <= interval_end && interval_start <= hd_end
+    then
+      merge_intervals (min hd_start interval_start, max hd_end interval_end) tl
+    else (hd_start, hd_end) :: merge_intervals (interval_start, interval_end) tl
+;;
+
+let rec make_merge_interval_list
+          (lines : String.t list)
+          (interval_ls : interval_list)
+  : interval_list
+  =
+  match lines with
+  | [] -> raise (Failure "")
+  | line :: tl ->
+    if String.is_empty line
+    then interval_ls
+    else (
+      let dash_pos = String.index line '-' in
+      match dash_pos with
+      | Some pos ->
+        let interval_start : int =
+          String.sub line ~pos:0 ~len:pos |> int_of_string
+        in
+        let interval_end : int =
+          String.sub line ~pos:(pos + 1) ~len:(String.length line - (pos + 1))
+          |> int_of_string
+        in
+        let interval : Interval.t = interval_start, interval_end in
+        merge_intervals interval interval_ls |> make_merge_interval_list tl
+      | None -> raise (Failure ""))
+;;
+
 let read_input filename =
   let lines = In_channel.read_lines filename in
   lines
 ;;
 
-make_interval_list (read_input "input.txt") []
+let filename = "input.txt" in
+make_interval_list (read_input filename) []
 |> count_valid_ids
+|> string_of_int
+|> print_endline;
+(* day 2 *)
+let intervals = make_merge_interval_list (read_input filename) [] in
+List.fold intervals ~init:0 ~f:(fun total (st, nd) -> total + nd - st + 1)
 |> string_of_int
 |> print_endline
