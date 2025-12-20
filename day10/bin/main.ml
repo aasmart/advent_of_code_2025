d60f1afa4f0c1dc60e142a68d662d138ba9c7150
Alexander Smart <aasmart@umich.edu>
2025-12-20 13:22:10 -0500

    day 10 pt2 ilp

--- day10/bin/main.ml
+++ day10/bin/main.ml
@@ -3,6 +3,8 @@ open Core
 let read_lines filname = In_channel.read_lines filname
 let lights_regex = Re2.create_exn "(\\[[.#]+\\])"
 let toggle_regex = Re2.create_exn "(\\([0-9,]*\\))"
+let joltage_regex = Re2.create_exn "({[0-9,]*})"
+let num_regex = Re2.create_exn "(\\d*)"
 
 let part_1 filename =
   List.fold (read_lines filename) ~init:0 ~f:(fun total line ->
@@ -62,5 +64,72 @@ let part_1 filename =
   |> print_endline
 ;;
 
+let solve problem =
+  (* For other interfaces, use Lp_glpk_js or Lp_gurobi instead *)
+  match Lp_glpk.solve ~term_output:false problem with
+  | Ok (_, xs) ->
+    Lp.PMap.bindings xs
+    |> List.fold ~init:0. ~f:(fun acc (_, res) -> acc +. res)
+    |> int_of_float
+  | Error msg -> failwith msg
+;;
+
+(* each button is basically a column in a matrix, and the target is the joltage values *)
+(* the number of times we press the button is the scale factor of the column *)
+let part_2 filename =
+  List.fold (read_lines filename) ~init:0 ~f:(fun total line ->
+    let extract_digit_list str =
+      Re2.find_all_exn num_regex str
+      |> List.filter ~f:(fun s -> String.is_empty s |> not)
+      |> List.map ~f:int_of_string
+    in
+    let toggle_sets =
+      match Re2.find_all toggle_regex line with
+      | Ok toggles -> List.map toggles ~f:extract_digit_list
+      | Error _ -> failwith "no toggles"
+    in
+    let joltage_requirements =
+      Option.value_exn (Re2.find_submatches_exn joltage_regex line).(0)
+      |> extract_digit_list
+    in
+    (* create the problem we want to optimize *)
+    let problem =
+      let lp_vars =
+        List.init (List.length toggle_sets) ~f:(fun i ->
+          Lp.var
+            ~integer:true
+            ~lb:0.
+            (String.of_char (Char.of_int_exn (i + 65))))
+      in
+      (* we want to minimize the sum of the number of times we press each button*)
+      let objective =
+        Lp.minimize
+          (List.reduce_exn lp_vars ~f:(fun curr_poly next ->
+             Lp.( ++ ) curr_poly next))
+      in
+      (* build the polynomial portion of the constraints *)
+      let constraint_polys =
+        Array.create ~len:(List.length joltage_requirements) (Lp.c 0.)
+      in
+      List.iter2_exn toggle_sets lp_vars ~f:(fun set var ->
+        List.iter set ~f:(fun counter ->
+          constraint_polys.(counter) <- Lp.( ++ ) constraint_polys.(counter) var));
+      (* add the joltage equivalence to each constraint *)
+      let constraints =
+        List.map2_exn
+          (Array.to_list constraint_polys)
+          joltage_requirements
+          ~f:(fun poly joltage ->
+            let c = Lp.( =~ ) poly (Lp.c (joltage |> float_of_int)) in
+            c)
+      in
+      Lp.make objective constraints
+    in
+    total + solve problem)
+(* in *)
+(*     ; *)
+;;
+
 let filename = "input.txt" in
-part_1 filename
+part_1 filename;
+part_2 filename |> string_of_int |> print_endline
